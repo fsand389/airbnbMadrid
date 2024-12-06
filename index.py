c73dd69d8c7a5e5b6d48f02af2c6c0d6dc737652
fsand389 <fsand389@mtroyal.ca>
2024-12-06 16:01:49 -0700

    create logic to check if db exists, if not then put csv into a data dictionary. then the data dictionary inserts into mongodb collection. after creating it, it will only say database already exists. however once we add queries it will let us print whatever we need.

--- index.py
+++ index.py
@@ -7,5 +7,29 @@ import pprint
 #Connect to Local MongoDB Server
 database = pymongo.MongoClient("mongodb://localhost:27017/")
 
+#creating variable with list of existing database names
+dbList = database.list_database_names()
 
+#Check if database already Exists 
+if "TheDatabase" not in dbList:
+    print("Creating new Database")
+    mydb = database["TheDatabase"]
+    print(database.list_database_names())
 
+    #put csv info into a data dict 
+    with open("listings.csv",'r', encoding='utf-8') as file:
+        csv_read = csv.DictReader(file)
+        data = [row for row in csv_read]
+
+    #put data dict info into mongodb collection
+    collection = mydb["listings"]
+    if "listings" in mydb.list_collection_names():
+        print("Collection already exists")
+    collection.insert_many(data)
+    
+    
+else:
+    print("Database already exists")
+    mydb = database["TheDatabase"]
+    collection = mydb["listings"]
+    
